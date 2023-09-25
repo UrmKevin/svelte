74306b3f362911998d6e4044518f3d2494df3c59
kevin <kevinurm2005@gmail.com>
2023-09-25 14:46:35 +0300

    Task 2 completed

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -50,6 +50,6 @@
 
 <nav>
 	<ul>
-		<li><a aria-current="{segment === 'registr' ? 'page' : undefined}" href="registr">registr</a></li>
+		<li><a aria-current="{segment === 'register' ? 'page' : undefined}" href="register">register</a></li>
 	</ul>
 </nav>
